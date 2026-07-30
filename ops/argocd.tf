100b9d858bc92b77f1d522d8594a18f9130d6f2e
The0Mikkel <developer@themikkel.dk>
2026-07-30 17:06:49 +0200

    refactor(argocd): standardize local variable naming for replica counts and HA settings

--- ops/argocd.tf
+++ ops/argocd.tf
@@ -5,11 +5,11 @@ resource "kubernetes_namespace_v1" "argocd" {
 }
 
 locals {
-  argocdRedisHaEnabled = var.argocd_redis_ha != null ? var.argocd_redis_ha : var.deployment_type == "ha"
-  argocdControllerReplicas = var.argocd_controller_replicas != null ? var.argocd_controller_replicas : 1
-  argocdServerReplicas = var.argocd_server_replicas != null ? var.argocd_server_replicas : (var.deployment_type == "single-node" ? 1 : 2)
-  argocdRepoServerReplicas = var.argocd_repo_server_replicas != null ? var.argocd_repo_server_replicas : (var.deployment_type == "single-node" ? 1 : 2)
-  argocdApplicationSetReplicas = var.argocd_application_set_replicas != null ? var.argocd_application_set_replicas : (var.deployment_type == "single-node" ? 1 : 2)
+  argocd_redis_ha_enabled      = var.argocd_redis_ha != null ? var.argocd_redis_ha : var.deployment_type == "ha"
+  argocd_controller_replicas     = var.argocd_controller_replicas != null ? var.argocd_controller_replicas : 1
+  argocd_server_replicas         = var.argocd_server_replicas != null ? var.argocd_server_replicas : (var.deployment_type == "single-node" ? 1 : 2)
+  argocd_repoServer_replicas     = var.argocd_repo_server_replicas != null ? var.argocd_repo_server_replicas : (var.deployment_type == "single-node" ? 1 : 2)
+  argocd_application_set_replicas = var.argocd_application_set_replicas != null ? var.argocd_application_set_replicas : (var.deployment_type == "single-node" ? 1 : 2)
 }
 
 resource "helm_release" "argocd" {
@@ -28,19 +28,19 @@ resource "helm_release" "argocd" {
   values = [
     yamlencode({
       "redis-ha" = {
-        enabled = local.argocdRedisHaEnabled
+        enabled = local.argocd_redis_ha_enabled
       },
       controller = {
-        replicas : local.argocdControllerReplicas
+        replicas : local.argocd_controller_replicas
       },
       server = {
-        replicas : local.argocdServerReplicas
+        replicas : local.argocd_server_replicas
       },
       repoServer = {
-        replicas : local.argocdRepoServerReplicas
+        replicas : local.argocd_repoServer_replicas
       },
       applicationSet = {
-        replicas : local.argocdApplicationSetReplicas
+        replicas : local.argocd_application_set_replicas
       }
     }),
   ]
