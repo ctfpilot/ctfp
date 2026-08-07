ac8503fdb9693bd7eb126c0d4ea6ab1a4ca9d04a
The0Mikkel <developer@themikkel.dk>
2026-08-08 01:22:48 +0200

    feat(cloudflare): refactor DNS to correctly decide when to proxy
    Cloudflare by default only covers the TLD and one level of subdomains. The system automatically detects the number of levels, and decides if the record should be proxied based on it Cloudflare allows it.

--- cluster/cloudflare.tf
+++ cluster/cloudflare.tf
@@ -2,6 +2,26 @@
 # DNS
 # ----------------------
 
+locals {
+  management_dns = "${var.cluster_dns_management}"
+  management_dns_subdomain = replace(var.cluster_dns_management, "${var.cloudflare_dns_management}", "")
+  management_dns_should_proxy = length(split(".", local.management_dns_subdomain)) <= 2 ? true : false
+  management_dns_wildcard = "*.${var.cluster_dns_management}"
+  management_dns_wildcard_should_proxy = length(split(".", local.management_dns_subdomain)) < 2 ? true : false
+
+  platform_dns   = "${var.cluster_dns_platform}"
+  platform_dns_subdomain = replace(var.cluster_dns_platform, "${var.cloudflare_dns_platform}", "")
+  platform_dns_should_proxy = length(split(".", local.platform_dns_subdomain)) <= 2 ? true : false
+  platform_dns_wildcard = "*.${var.cluster_dns_platform}"
+  platform_dns_wildcard_should_proxy = length(split(".", local.platform_dns_subdomain)) < 2 ? true : false
+
+  ctf_dns        = "${var.cluster_dns_ctf}"
+  ctf_dns_subdomain = replace(var.cluster_dns_ctf, "${var.cloudflare_dns_ctf}", "")
+  ctf_dns_should_proxy = length(split(".", local.ctf_dns_subdomain)) <= 2 ? true : false
+  ctf_dns_wildcard = "*.${var.cluster_dns_ctf}"
+  ctf_dns_wildcard_should_proxy = length(split(".", local.ctf_dns_subdomain)) < 2 ? true : false
+}
+
 data "cloudflare_zones" "domain_name_zone_management" {
   filter {
     name = var.cloudflare_dns_management
@@ -11,10 +31,11 @@ data "cloudflare_zones" "domain_name_zone_management" {
 # Create DNS A record
 resource "cloudflare_record" "domain_name_management" {
   zone_id = data.cloudflare_zones.domain_name_zone_management.zones.0.id
-  name    = var.cluster_dns_management
+  name    = local.management_dns
   content = module.kube-hetzner.ingress_public_ipv4
   type    = "A"
   ttl     = 1
+  proxied = local.management_dns_should_proxy
 
   depends_on = [
     data.cloudflare_zones.domain_name_zone_management,
@@ -24,10 +45,11 @@ resource "cloudflare_record" "domain_name_management" {
 # Create DNS A wildcard record
 resource "cloudflare_record" "wildcard_domain_name_management" {
   zone_id = data.cloudflare_zones.domain_name_zone_management.zones.0.id
-  name    = "*.${var.cluster_dns_management}"
+  name    = local.management_dns_wildcard
   content = var.cluster_dns_management
   type    = "CNAME"
   ttl     = 1
+  proxied = local.management_dns_wildcard_should_proxy
 
   depends_on = [
     data.cloudflare_zones.domain_name_zone_management,
@@ -43,11 +65,11 @@ data "cloudflare_zones" "domain_name_zone_ctf" {
 # Create DNS A record
 resource "cloudflare_record" "domain_name_ctf" {
   zone_id = data.cloudflare_zones.domain_name_zone_ctf.zones.0.id
-  name    = var.cluster_dns_ctf
+  name    = local.ctf_dns
   content = module.kube-hetzner.ingress_public_ipv4
   type    = "A"
   ttl     = 1
-  proxied = true
+  proxied = local.ctf_dns_should_proxy
 
   depends_on = [
     data.cloudflare_zones.domain_name_zone_ctf,
@@ -57,11 +79,11 @@ resource "cloudflare_record" "domain_name_ctf" {
 # Create DNS A wildcard record
 resource "cloudflare_record" "wildcard_domain_name_ctf" {
   zone_id = data.cloudflare_zones.domain_name_zone_ctf.zones.0.id
-  name    = "*.${var.cluster_dns_ctf}"
+  name    = local.ctf_dns_wildcard
   content = module.kube-hetzner.ingress_public_ipv4
   type    = "A"
   ttl     = 1
-  proxied = false
+  proxied = local.ctf_dns_wildcard_should_proxy
 
   depends_on = [
     data.cloudflare_zones.domain_name_zone_ctf,
@@ -77,11 +99,11 @@ data "cloudflare_zones" "domain_name_zone_platform" {
 # Create DNS A record
 resource "cloudflare_record" "domain_name_platform" {
   zone_id = data.cloudflare_zones.domain_name_zone_platform.zones.0.id
-  name    = var.cluster_dns_platform
+  name    = local.platform_dns
   content = module.kube-hetzner.ingress_public_ipv4
   type    = "A"
   ttl     = 1
-  proxied = true
+  proxied = local.platform_dns_should_proxy
 
   depends_on = [
     data.cloudflare_zones.domain_name_zone_platform,
@@ -91,11 +113,11 @@ resource "cloudflare_record" "domain_name_platform" {
 # Create DNS A wildcard record
 resource "cloudflare_record" "wildcard_domain_name_platform" {
   zone_id = data.cloudflare_zones.domain_name_zone_platform.zones.0.id
-  name    = "*.${var.cluster_dns_platform}"
+  name    = local.platform_dns_wildcard
   content = module.kube-hetzner.ingress_public_ipv4
   type    = "A"
   ttl     = 1
-  proxied = true
+  proxied = local.platform_dns_wildcard_should_proxy
 
   depends_on = [
     data.cloudflare_zones.domain_name_zone_platform,
